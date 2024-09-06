3cc1eaf2e826ae5ef60d4f041bd11ffb3229b7e3
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-09-06 14:28:21 -0300

    Inserido configuração do EF Core e adicionado string de conexão

---
+++ APICatalogo/Context/APIWebContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class APIWebContext : DbContext
+{
+
+    public APIWebContext(DbContextOptions<APIWebContext> options) : base(options)
+    {
+           
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }    
+    public DbSet<Produto>? Produtos { get; set; }    
+}
