ed39830d8b93c3534eed5b8c15727c3dbbec8633
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-09-09 11:51:01 -0300

    Inserindo

---
+++ APICatalogo/Migrations/20240909132219_PopulaCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias(Nome,ImagemUrl) Values('Bebida','bebidas.jpg')");
+            mb.Sql("insert into Categorias(Nome,ImagemUrl) Values('Lanches','lanches.jpg')");
+            mb.Sql("insert into Categorias(Nome,ImagemUrl) Values('Sobremesas','sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
