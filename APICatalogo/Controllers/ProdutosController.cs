75e6a7218e36b6f529a838ba9ce856144baf8b91
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-09-10 10:36:34 -0300

    Criado metodos http de produtos

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,7 +1,9 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Models;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -15,5 +17,66 @@ namespace APICatalogo.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get() 
+        { 
+            var produtos = _context.Produtos.ToList();
+            if(produtos is null)
+            {
+                return NotFound();
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", 
+                new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+                return NotFound();
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
