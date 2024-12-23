bc3f7d927d3f63fe7637cf3f81b8c84f23530de1
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-12-23 20:36:56 -0300

    Adicionando connectionString em arquivo local

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,19 +1,27 @@
 using System.Text.Json.Serialization;
-
 using APICatalogo.Context;
 using APICatalogo.Extensions;
 using APICatalogo.Filters;
 using APICatalogo.Interface;
 using APICatalogo.Repository;
-using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using NuGet.Protocol.Core.Types;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Referencia Ciclica
-builder.Services.AddControllers().AddJsonOptions(options => 
+// Load configuration from appsettings.json
+var configuration = builder.Configuration;
+
+// Get the path of the connection strings file from appsettings.json
+string connectionStringsFilePath = configuration["ConnectionStringsFilePath"];
+
+// Load connection strings from the specified XML file
+builder.Configuration.AddXmlFile(connectionStringsFilePath, optional: false, reloadOnChange: true);
+
+// Referencia Ciclica
+builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions
     .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
@@ -23,7 +31,7 @@ builder.Services.AddSwaggerGen();
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<APIWebContext>(options =>
-    options.UseMySql(mySqlConnection ,ServerVersion.AutoDetect(mySqlConnection)));
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddScoped<ApiLoggingFilter>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -47,7 +55,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     app.ConfigureExceptionHandler();
-    
 }
 
 app.UseHttpsRedirection();
