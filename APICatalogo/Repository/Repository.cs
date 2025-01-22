e0cb7ff509ac0e451a30aa1b37be6b6076f20ae1
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-01-22 09:01:08 -0300

    chore: Alteração da class Repository

--- APICatalogo/Repository/Repository.cs
+++ APICatalogo/Repository/Repository.cs
@@ -18,7 +18,7 @@ public class Repository<T> : IRepository<T> where T : class
 
     public List<T> GetAll()
     {
-        return _context.Set<T>().ToList();
+        return _context.Set<T>().AsNoTracking().ToList();
     }
 
     public T? Get(Expression<Func<T, bool>> predicate)
@@ -30,21 +30,21 @@ public class Repository<T> : IRepository<T> where T : class
     public T Create(T entity)
     {
         _context.Set<T>().Add(entity);
-        _context.SaveChanges();
+        
         return entity;
     }
 
     public T Update(T entity)
     {
         _context.Set<T>().Update(entity);
-        _context.SaveChanges();
+       
         return entity;
     }
 
     public T Delete(T entity)
     {
         _context.Set<T>().Remove(entity);
-        _context.SaveChanges();
+        
         return entity;
     }
 }
